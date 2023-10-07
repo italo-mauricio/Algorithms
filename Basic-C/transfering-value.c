09d2ef73f0478a8f2a9e7aec9858661b4d3f5fb3
italo <italomauricio98@gmail.com>
2023-10-07 19:26:07 -0300

    update

---
+++ Basic-C/transfering-value.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a = 10;
+    int *p1 = NULL;
+    int *p2;
+
+    p1 = &a;
+    p2 = p1;
+    *p2 = 4;
+
+    printf("a = %d", a);
+
+    return 0;
+}

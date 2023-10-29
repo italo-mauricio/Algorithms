529252f7a6ae7c851551da430b7c02dbc81536a2
italo <italomauricio98@gmail.com>
2023-10-29 14:41:22 -0300

    update

--- Two-Sum/TwoSum.java
+++ Two-Sum/TwoSum.java
@@ -4,16 +4,14 @@ import java.util.Map;
 class TwoSum{
    public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
-        int[] output = new int[2];
 
         for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) {
-                output[0] = map.get(nums[i]);
-                output[1] = i;
-                return output;
+            Integer complementeIndex = map.get(nums[i]);
+            if(complementeIndex != null){
+                return new int[]{i, complementeIndex};
             }
             map.put(target - nums[i], i);
-        }
-        return null;
+        }   
+        return nums;
     }
 }
